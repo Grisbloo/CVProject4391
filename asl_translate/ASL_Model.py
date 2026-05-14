3cb1a6a6315870710e3b759a6260a6694a751668
Bloo <nightshadowsilencer@gmail.com>
2026-05-14 12:14:48 -0500

    Update ASL_Model.py

--- asl_translate/ASL_Model.py
+++ asl_translate/ASL_Model.py
@@ -8,14 +8,14 @@ class ASLSequenceInterpreter(nn.Module):
     def __init__(self, input_size=63, hidden_size=128, num_layers=2, num_classes=26):
         super(ASLSequenceInterpreter, self).__init__()
         
-        # --- THE MEMORY LAYER (LSTM) ---
+        # THE MEMORY LAYER (LSTM) 
         # input_size = 63 (because 21 MediaPipe joints * 3 coordinates (x,y,z))
         # hidden_size = 64 (the number of "neurons" parsing the patterns)
-        # batch_first=True tells PyTorch our data is structured as (Batch, Sequence, Features)
+        # batch_first=True tells PyTorch data is structured as (Batch, Sequence, Features)
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         
         # THE DECISION LAYER
-        # After the LSTM processes the sequence, we squish the data down to make a guess
+        # Processes the sequence, and force the model to make a guess
         self.fc1 = nn.Linear(hidden_size, 64)
         self.relu = nn.ReLU() # Activation function (keeps the math non-linear)
         self.dropout = nn.Dropout(0.3) # Prevents the LSTM from just memorizing the data
